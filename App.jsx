4a17f9c8c56d1e3abff4c802dda9422656274ac4
Edwin-a75 <edwinasare65@gmail.com>
2025-06-14 14:32:14 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function App() {
+  return <div className='text-xl font-bold'>Welcome to Edwin's Reseller Hub</div>;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Edwin-s-Reseller-Hub
+# Edwin's Reseller Hub
+
+This is a mockup for a personal brand and resale business website.
